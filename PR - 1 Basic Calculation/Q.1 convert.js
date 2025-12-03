5e5fc0a4b1cd0d02cc8c577316ccdd020154b58a
Pr@gna <ahirpragna14@gmail.com>
2025-12-03 23:02:27 +0530

    pr-1

---
+++ PR - 1 Basic Calculation/Q.1 convert.js
@@ -0,0 +1,8 @@
+console.log("Temperature");
+
+let celsius = +prompt("Enter any temperature : ");
+
+let fahrenheit = +(celsius * 9 / 5) + 32;
+
+console.log("Temperature in Fahrenheit : " , fahrenheit);
+document.write("Temperature in Fahrenheit : " , fahrenheit);
